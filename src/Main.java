3a8e9eb91c30866aa5b88277997e425aafffa5dd
KrystsinaPankevich <kristinapankevic21@gmail.com>
2025-05-31 12:13:46 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java is a compiled programming language."); //  It's important to remember.
+        System.out.println();
+        System.out.println("In Java, class names are only nouns\n and are written with a capital letter.");
+        /*
+        Это многострочный или блочный комментарий.
+        Он может занимать несколько строк
+         */
+    }
+}
